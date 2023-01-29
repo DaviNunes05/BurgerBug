2c9aadcf868a319e51c1962ff888185a0914465d
DaviNunes05 <davi.nunes05@gmail.com>
2023-01-29 13:48:00 -0300

    Adicionando Projeto

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <Navbar :logoVindoDoPai="logo_src" :altVindoDoPai="app_name" />
+    <router-view />
+    <Footer />
+</template>
+<script>
+import Navbar from './components/Navbar.vue';
+import Footer from './components/Footer.vue';
+
+export default {
+    components: {
+        Navbar,
+        Footer
+    },
+    data(){
+        return{
+            logo_src: "/img/logo.png",
+            app_name: "Monte seu BurgerBug"
+        }
+    }
+}
+</script>
---
+++ src/components/Banner.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="main-banner">
+        <h1>Monte seu BurguerBug</h1>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+    #main-banner{
+        background-image: url('/img/burger.jpg');
+        background-position: 0 -250px;
+        background-size: cover;
+        height: 500px;
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+    }
+
+    #main-banner h1{
+        color: white;
+        text-align: center;
+        font-size: 40px;
+        background-color: #222;
+        padding: 20px 40px;
+    }
+</style>
